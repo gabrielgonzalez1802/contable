e5be53a5e9cab570a7d69058158d930409eeeda0
gabriel <gabrielgonzalez1802@gmail.com>
2020-10-06 09:01:29 -0400

    Ajuste e sesion en carpetas

--- src/main/java/com/contable/controller/CarpetasController.java
+++ src/main/java/com/contable/controller/CarpetasController.java
@@ -1,5 +1,7 @@
 package com.contable.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +28,8 @@ public class CarpetasController {
 	public ResponseEntity<String> buscarCarpeta(@PathVariable("carpeta") String item, HttpSession session){
 		Carpeta carpeta = serviceCarpetas.buscarPorNombre(item);
 		if(carpeta == null) {
-			session.setAttribute("carpeta", null);
+			List<Carpeta> carpetas = serviceCarpetas.buscarTipoCarpeta(1);
+			session.setAttribute("carpeta", carpetas.get(0).getId());
 			return new ResponseEntity<>("No tienes carpetas creadas", HttpStatus.OK);
 		}
 		session.setAttribute("carpeta", carpeta.getId());
