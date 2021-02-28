ef85e5aacdb3d15592cad4cbca02223b5343ac9b
gabriel <gabrielgonzalez1802@gmail.com>
2021-02-27 21:13:44 -0400

    Ajuste de hora en proceso cron

--- src/main/java/com/contable/cron/PrestamosCron.java
+++ src/main/java/com/contable/cron/PrestamosCron.java
@@ -61,7 +61,7 @@ public class PrestamosCron {
 	private final Integer VENCIDO = 2;
 	private final Integer LEGAL = 3;
 
-	@Scheduled(cron = "0 10 21 * * *")
+	@Scheduled(cron = "0 00 03 * * *")
 	public void calculoVencimientoCuota() throws ParseException {		
 		//Buscamos los detalles pendientes de los prestamos 
 		List<PrestamoDetalle> prestamoDetallesTemp = servicePrestamosDetalles.buscarPorEstado(0);
